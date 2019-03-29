e1a178f145be99b7bf83c404e4e670e8cbccc3e4
philFernandez <norcal618@gmail.com>
2019-03-28 22:50:16 -0700

    uline 9 Flor Romero
    Signed-off-by: FlorRomero<florromero@csus.edu>

--- Lab02/src/Hello.java
+++ Lab02/src/Hello.java
@@ -1,12 +1,12 @@
-
 public class Hello {
 
+	
 	public static void main(String[] args) {
 		System.out.println("Welcome to Git!");
 		System.out.println("yang - This line was added by Dr. Yang.");
 		// Add a new line with your information
-		System.out.println("SacStateID - This line was added by First name Last name.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("219845705 - This line was added by Philip Fernandez.");
+		System.out.println("201140788 - This line was added by Flor Romero.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
@@ -18,3 +18,4 @@ public class Hello {
 	}
 
 }
+ 
