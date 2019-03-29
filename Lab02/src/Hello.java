a53d12177a932109e4e426c47e0962e5697a4140
philFernandez <norcal618@gmail.com>
2019-03-28 22:29:52 -0700

    put code on same lines as professor copy

--- Lab02/src/Hello.java
+++ Lab02/src/Hello.java
@@ -1,8 +1,9 @@
+
 public class Hello {
+
 	public static void main(String[] args) {
 		System.out.println("Welcome to Git!");
 		System.out.println("yang - This line was added by Dr. Yang.");
-		
 		// Add a new line with your information
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
@@ -13,5 +14,7 @@ public class Hello {
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
+
 	}
+
 }
