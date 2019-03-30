1747b4907189904fb730fa4542d92c5c8ed85d22
romer <romer@DESKTOP-BFKRLUB>
2019-03-29 18:54:50 -0700

    Line 9 was change

--- Lab02/src/Hello.java
+++ Lab02/src/Hello.java
@@ -6,7 +6,7 @@ public class Hello {
 		System.out.println("yang - This line was added by Dr. Yang.");
 		// Add a new line with your information
 		System.out.println("219845705 - This line was added by Philip Fernandez.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("201140788- This line was added by Flor Romero.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
