2a455fea15de129f1098c8778257c630baadb60c
khris <khris@MSI>
2019-04-01 17:19:10 -0800

    Not sure if this is correct.

--- Lab02/src/Hello.java
+++ Lab02/src/Hello.java
@@ -7,7 +7,7 @@ public class Hello {
 		// Add a new line with your information
 		System.out.println("219845705 - This line was added by Philip Fernandez.");
 		System.out.println("201140788- This line was added by Flor Romero.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("213725487 - This line was added by Nikolay Khristyuchenko.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
