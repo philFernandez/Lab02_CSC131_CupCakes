2e5985cad15e24da6257d09e22cefe0c8283666e
Ken Huynh <Ken Huynh@10.117.82.3>
2019-04-03 18:43:47 -0700

    This was done by Nhan Huynh.

--- Lab02/src/Hello.java
+++ Lab02/src/Hello.java
@@ -9,7 +9,7 @@ public class Hello {
 		System.out.println("201140788- This line was added by Flor Romero.");
 		System.out.println("213725487 - This line was added by Nikolay Khristyuchenko.");
 		System.out.println("220145966 - This line was added by FranzJrard Colcol.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("219885875 - This line was added by Nhan Huynh.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
