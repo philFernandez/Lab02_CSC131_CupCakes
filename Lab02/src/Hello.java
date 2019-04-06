42a9dec9a34ad15a13db31269876caa8b9e8d2b5
awkwardAlpha <47482598+awkwardAlpha@users.noreply.github.com>
2019-04-05 22:27:04 -0700

    Added name and ID - Patrick Eder

--- Lab02/src/Hello.java
+++ Lab02/src/Hello.java
@@ -10,7 +10,7 @@ public class Hello {
 		System.out.println("213725487 - This line was added by Nikolay Khristyuchenko.");
 		System.out.println("220145966 - This line was added by FranzJrard Colcol.");
 		System.out.println("219885875 - This line was added by Nhan Huynh.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("217451742 - This line was added by Patrick Eder.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
